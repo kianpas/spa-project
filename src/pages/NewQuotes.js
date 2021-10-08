e1204bfd6c2df9043fe216454269a497bc83301a
kianpas <kianpas@gmail.com>
2021-10-09 01:02:45 +0900

    2021.10.09 sort, prompt commit

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -1,8 +1,15 @@
+import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuotes = (params) => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
       console.log(quoteData);
+
+      //push 히스토리에 새 페이지를 추가입력, 뒤로가기 가능
+      //replace 교체, 뒤로가기 불가능, 리다이렉트 같은 것
+      history.push("/quotes")
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
