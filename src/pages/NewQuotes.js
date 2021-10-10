7b2f8dcc109e635ece8ecd0d673b61883f0fe915
kianpas <kianpas@gmail.com>
2021-10-10 22:04:42 +0900

    2021.10.10 Lazy Loading & firebase deploy commit

--- src/pages/NewQuotes.js
+++ src/pages/NewQuotes.js
@@ -5,7 +5,7 @@ import QuoteForm from "../components/quotes/QuoteForm";
 import useHttp from "../hooks/use-http";
 import { addQuote } from "../lib/api";
 
-const NewQuotes = (p) => {
+const NewQuotes = () => {
   const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
 
